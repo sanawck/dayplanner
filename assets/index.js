c5ea9e52919b0b104e817b166580bbd01bd076e6
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-03-21 11:25:20 -0400

    corrected errors in save Btn - planner should be working now

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,9 @@
 $(document).ready(function () {
     // Hours of the day variable
-    let workHours = ["9:00 am","10:00 am","11:00 am", "12:00 pm", "1:00 pm","2:00 pm","3:00 pm","4:00 pm","5:00 pm",];
+    let workHours = ["9:00 am","10:00 am","11:00 am", "12:00 pm", "1:00 pm","2:00 pm","3:00 pm","4:00 pm","5:00 pm"];
 
 // displays current day
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-// For Loop with JQuery draws row hours with assigned classes and IDs
-    for (i = 0; i < workHours.length; i++) {
-        console.log(workHours[i]);
-        $(`<div class="col-2 time-block"></div>`)
-            .text(workHours[i])
-            .appendTo(".row");
-        $(`<textarea class="col-8 note-input" id="input-${i}"></textarea>`)
-            .attr("placeholder", "Enter Tasks Here")
-            .appendTo(".row");
-        $(`<button class="col-2 btn btn-secondary saveBtn" id="hour${i}"></button>`)
-            .text("save")
-            .appendTo(".row");
-    }
 
 // Moment.js to get the current date and time
     let now = new Date().getHours();
@@ -28,7 +15,7 @@ $(".saveBtn").on("click", function(){
     let time = $(this).parent().attr("id");
 
     localStorage.setItem(time, activity);
-
+});
 
 // Determine if each row is past, present, or future 
 function updateActivity() {
@@ -53,6 +40,7 @@ function updateActivity() {
 }
 updateActivity();
 //check if every 15 minutes the activity updates according to time
+let interval = setInterval(updateActivity, 1000);
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -62,4 +50,6 @@ $("#hour-14 .description").val(localStorage.getItem("hour-14"));
 $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-}
+
+});
+
