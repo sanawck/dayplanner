0d7df6e6b78c6dc88c59265b53c3405bef9921aa
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-03-21 10:59:22 -0400

    added save btn function

--- assets/index.js
+++ assets/index.js
@@ -22,9 +22,44 @@ $(document).ready(function () {
     let now = new Date().getHours();
 
 
-// Determine if each row is past, present, or future 
-
 //store  data to local storage
+$(".saveBtn").on("click", function(){
+    let activity = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
 
-});
+    localStorage.setItem(time, activity);
 
+
+// Determine if each row is past, present, or future 
+function updateActivity() {
+    let currentHour = moment().hours();
+
+    $(".time-block").each(function(){
+     let activityHour = parseInt($(this).attr("id").split("-")[1]);
+
+     if (activityHour < currentHour){
+        $(this).addClass("past");
+     }
+     else if (activityHour === currentHour){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+     }
+     else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+     }
+    });
+}
+updateActivity();
+//check if every 15 minutes the activity updates according to time
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+}
